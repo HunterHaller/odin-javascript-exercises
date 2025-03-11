afcd241ba825084cee400bab49c379f255716bbf
HunterHaller <huntinghollers@proton.me>
2025-03-11 16:31:05 +0100

    Solved exercises 1-4

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(forwardInput) {
+    let reverseInput = "";
+    let inputEnd = forwardInput.length - 1; //The final index of the input string
+    for (let i = 0; i < forwardInput.length; i++){
+        reverseInput = reverseInput + forwardInput[inputEnd]
+        inputEnd--
+    }
+    return reverseInput;
 };
 
 // Do not edit below this line
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,25 @@
-const removeFromArray = function() {
+const removeFromArray = function(inputArray, ...removeMe) { //Function accepts an indefinite number of arguments
+
+    let removeThese = Array.from(removeMe); //Create array from list of items to be removed
+    
+    //console.log("removeThese array = " + removeThese)
+
+    for (let i = 0; i < inputArray.length; i++){
+        //console.log("Checking " + inputArray[i] + " in inputArray...")
+        for (let j = 0; j < removeThese.length; j++){
+            //console.log("Checking " + inputArray[i] + " against " + removeThese[j])
+
+            if (inputArray[i] === removeThese[j]){
+                inputArray.splice(i, 1) //Aha! If left unchecked, this breaks the for loop!
+                i-- //I must decrease i to make sure all elements are checked.
+                //console.log("!!Removed " + removeThese[j] + " from inputArray!!")
+            }
+            else{
+                //console.log("Didn't remove " + inputArray[i]    )
+            }
+        }
+    }
+    return inputArray;
 };
 
 // Do not edit below this line
