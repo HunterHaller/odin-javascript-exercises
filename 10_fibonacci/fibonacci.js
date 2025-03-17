099201fc2ed737d73b7ac09c60947701f3886069
HunterHaller <huntinghollers@proton.me>
2025-03-17 15:28:46 +0100

    Solved 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,20 @@
-const fibonacci = function() {
+const fibonacci = function(member) {
+    let sequenceCount = 0;
+    let prevCount = 1;
+    let totalCount = 0;
 
+    if (member < 0){
+        return "OOPS"
+    }
+
+    for (let i = 0; i != member; i++){
+        sequenceCount = totalCount;
+        totalCount = sequenceCount + prevCount;
+        prevCount = sequenceCount;
+        console.log(totalCount)
+    }
+
+    return totalCount;
 };
 
 // Do not edit below this line
