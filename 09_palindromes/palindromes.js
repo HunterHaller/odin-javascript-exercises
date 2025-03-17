6c301baecb3dc2282c31bc8448eb5427e8008947
HunterHaller <huntinghollers@proton.me>
2025-03-17 15:09:46 +0100

    Solved 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,21 @@
-const palindromes = function () {
+const palindromes = function (inputArray) {
+    
+    let punctuation = ".!@#$%^&*()-_+=`~[]{}\|,./<> ";
 
+    let loweredInput = inputArray.toLowerCase();
+    let arrayInput = loweredInput.split("");
+    let noPunct = arrayInput.filter((letter) => !punctuation.includes(letter)) //Only keep characters that are not punctuation or spaces
+    let reversedInput = noPunct.toReversed(); //Found researching things, this creates a copy of the array, while reverse() uses a reference.
+    console.log("Normal array is " + noPunct)
+    console.log("Reversed array is " + reversedInput)
+
+    for (let i = 0; i < inputArray.length; i++){
+        if (noPunct[i] != reversedInput[i]){
+            return false;
+        }
+    }
+    //If we got through the for loop without returning false, then...
+    return true;
 };
 
 // Do not edit below this line
